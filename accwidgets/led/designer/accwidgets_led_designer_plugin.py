a97f52cee88ce298484e20ab98d9a0920fdcc247
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-09-16 14:17:48 +0200

    Fix typo

--- accwidgets/led/designer/accwidgets_led_designer_plugin.py
+++ accwidgets/led/designer/accwidgets_led_designer_plugin.py
@@ -10,6 +10,6 @@ from accwidgets._designer_base import create_plugin, WidgetBoxGroup
 _ICON_BASE_PATH = Path(__file__).parent.absolute()
 
 
-PropertyEditPlugin = create_plugin(widget_class=Led,
-                                   group=WidgetBoxGroup.INDICATORS,
-                                   icon_base_path=_ICON_BASE_PATH)
+LedPlugin = create_plugin(widget_class=Led,
+                          group=WidgetBoxGroup.INDICATORS,
+                          icon_base_path=_ICON_BASE_PATH)
