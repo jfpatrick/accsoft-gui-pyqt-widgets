037ec6d90be746360b0de473f691439f062c9b64
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-11-03 17:28:19 +0100

    Adjust the public module path for facaded APIs so that *.ui files and sphinx docs refer to the public API, not exact private package

--- accwidgets/property_edit/__init__.py
+++ accwidgets/property_edit/__init__.py
@@ -6,3 +6,10 @@
 
 from .propedit import (PropertyEdit, PropertyEditField, EnumItemConfig,
                        AbstractPropertyEditLayoutDelegate, AbstractPropertyEditWidgetDelegate)
+
+
+from accwidgets._api import mark_public_api
+mark_public_api(PropertyEdit, __name__)
+mark_public_api(PropertyEditField, __name__)
+mark_public_api(AbstractPropertyEditLayoutDelegate, __name__)
+mark_public_api(AbstractPropertyEditWidgetDelegate, __name__)
