271dd29a9cf80e3e94337b07016408a9bd94985c
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2020-11-10 18:14:59 +0100

    Do not load widgets in Qt Designer if runtime checks fail

--- accwidgets/led/designer/accwidgets_led_designer_plugin.py
+++ accwidgets/led/designer/accwidgets_led_designer_plugin.py
@@ -3,13 +3,20 @@ Module containing QtDesigner plugin for LED widget.
 """
 
 from pathlib import Path
-from accwidgets.led import Led
-from accwidgets._designer_base import create_plugin, WidgetBoxGroup
 
 
-_ICON_BASE_PATH = Path(__file__).parent.absolute()
+skip_plugin = False
+try:
+    from accwidgets.led import Led
+except ImportError:
+    skip_plugin = True
 
 
-LedPlugin = create_plugin(widget_class=Led,
-                          group=WidgetBoxGroup.INDICATORS,
-                          icon_base_path=_ICON_BASE_PATH)
+if not skip_plugin:
+    from accwidgets._designer_base import create_plugin, WidgetBoxGroup
+
+    _ICON_BASE_PATH = Path(__file__).parent.absolute()
+
+    LedPlugin = create_plugin(widget_class=Led,
+                              group=WidgetBoxGroup.INDICATORS,
+                              icon_base_path=_ICON_BASE_PATH)
