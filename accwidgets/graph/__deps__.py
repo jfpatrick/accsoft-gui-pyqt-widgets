361372195551425fa60ffad0a55e88e7d6443a7c
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-01-07 10:03:43 +0100

    Relax common dependencies to reduce collisions with user dependencies

--- accwidgets/graph/__deps__.py
+++ accwidgets/graph/__deps__.py
@@ -1,8 +1,8 @@
 core = [
-    "numpy>=1.16,<=1.20a0",
+    "numpy>=1.16,<=1.50a0",
     "scipy>=1.1,<2a0",
     "QtPy>=1.7,<2a0",
     "pyqtgraph==0.10.0.post0",
-    "qtawesome>=0.7.0,<0.8a0",
+    "qtawesome>=0.7.0,<2a0",
     "dataclasses>=0.7,<0.8a0;python_version<'3.7'",
 ]
