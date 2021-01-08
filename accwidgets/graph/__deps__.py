4b8a48b5b035bc4813ca14a824abc4ed3d6cdd04
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-01-08 17:31:55 +0100

    Update PyQtGraph to 0.11.0

--- accwidgets/graph/__deps__.py
+++ accwidgets/graph/__deps__.py
@@ -2,7 +2,7 @@ core = [
     "numpy>=1.16,<=1.50a0",
     "scipy>=1.1,<2a0",
     "QtPy>=1.7,<2a0",
-    "pyqtgraph==0.10.0.post0",
+    "pyqtgraph==0.11.0.post0",
     "qtawesome>=0.7.0,<2a0",
     "dataclasses>=0.7,<0.8a0;python_version<'3.7'",
 ]
