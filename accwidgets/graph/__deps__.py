7a011eb21e0208aa71a7d12763ef50df86dc9b5e
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-10-04 14:42:19 +0200

    Bump pyqtgraph to 0.12.2

--- accwidgets/graph/__deps__.py
+++ accwidgets/graph/__deps__.py
@@ -2,7 +2,7 @@ core = [
     "numpy>=1.16,<=1.50a0",
     "scipy>=1.1,<2a0",
     "QtPy>=1.7,<2a0",
-    "pyqtgraph>=0.11.1",
+    "pyqtgraph>=0.12.2",
     "qtawesome>=0.7.0,<2a0",
     "dataclasses>=0.7,<0.8a0;python_version<'3.7'",
 ]
