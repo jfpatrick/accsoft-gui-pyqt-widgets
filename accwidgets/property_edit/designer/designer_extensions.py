1dc6d5d65ce21a61005131eb7718cc2743ca74e0
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-10-15 17:25:22 +0200

    Fix PropertyEdit designer subdialogs being issued in a non-modal way

--- accwidgets/property_edit/designer/designer_extensions.py
+++ accwidgets/property_edit/designer/designer_extensions.py
@@ -228,7 +228,6 @@ class UserDataColumnDelegate(QStyledItemDelegate):
                                             on_save=functools.partial(self._save_from_numeric_dialog, index=regular_index),
                                             use_precision=field_type == PropertyEdit.ValueType.REAL,
                                             parent=self.parent())
-            dialog.show()
             dialog.exec_()
 
     def _save_from_enum_dialog(self, data: List[EnumItemConfig], index: QModelIndex):
