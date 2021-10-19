8526e62f88af121abd3faeb16c889fe90d59711f
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-10-19 16:26:01 +0200

    Bump packaging to 20.5 to support Python 3.9

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ REQUIREMENTS: Dict[str, List[str]] = {
     CORE_OPTION: [
         "QtPy>=1.10.0,<2a0",
         "qtawesome>=0.7.0,<2a0",
-        "packaging>=20.4,<20.5a0",
+        "packaging>=20.5,<22a0",
         "deprecated>=1.2.13,<1.5a0",
         "importlib-metadata>=1.7.0,<4.0a0;python_version<'3.8'",
     ],
