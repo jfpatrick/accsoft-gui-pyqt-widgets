3b37fcbcbe59e5f972331351393750cf41791614
Ivan Sinkarenko <ivan.sinkarenko@cern.ch>
2021-10-19 17:01:59 +0200

    Bump qasync version for Python 3.9 compatibility

--- accwidgets/parameter_selector/__deps__.py
+++ accwidgets/parameter_selector/__deps__.py
@@ -3,5 +3,5 @@ core = [
     "dataclasses>=0.7,<0.8a0;python_version<'3.7'",
     "qtawesome>=0.7.0,<2a0",
     "pyCCDA>=0.10.1.3.19,<0.11a0",
-    "qasync>=0.13.0,<1a0",
+    "qasync>=0.22.0,<1a0",
 ]
